5f1c13e7c4273fa5a4bf2e685237bdc7b7786e0c
921903719@qq.com <921903719@qq.com>
2025-12-27 17:07:21 +0800

    1.0.1

---
+++ SDK/LinWearKit.framework/Headers/LWCallback.h
@@ -0,0 +1,73 @@
+//
+//  LWCallback.h
+//  LinWearKit
+//
+//  Created by LINWEAR on 2025-12-16.
+//
+
+#ifndef LWCallback_h
+#define LWCallback_h
+
+/**
+ 结果回调
+ @param error               错误信息，为nil时表示成功
+ */
+typedef void(^LWResultCallback)(NSError * _Nullable error);
+
+
+/**
+ 数值结果回调
+ @param number              业务数值
+ @param error               错误信息，为nil时表示成功
+ */
+typedef void(^LWResultNumberCallback)(NSNumber * _Nullable number, NSError * _Nullable error);
+
+
+/**
+ 设备配置信息结果回调
+ @param object              设备配置信息
+ @param error               错误信息，为nil时表示成功
+ */
+typedef void(^LWResultDeviceConfigCallback)(LWDeviceConfigModel * _Nullable object, NSError * _Nullable error);
+
+
+/**
+ 宠物状态结果回调
+ @param object              宠物状态
+ @param error               错误信息，为nil时表示成功
+ */
+typedef void(^LWResultAipetStatusCallback)(LWAipetStatusModel * _Nullable object, NSError * _Nullable error);
+
+
+/**
+ 进度结果回调
+ @param progress            进度0-100
+ */
+typedef void(^LWResultProgressCallback)(int progress);
+
+
+/**
+ 离线语音授权码信息回调
+ @param object              离线语音授权码
+ @param error               错误信息，为nil时表示成功
+ */
+typedef void(^LWResultOfflineVoiceAuthCodeCallback)(NSString * _Nullable object, NSError * _Nullable error);
+
+
+/**
+ 星历文件信息回调
+ @param object              星历文件本地路径
+ @param error               错误信息，为nil时表示成功
+ */
+typedef void(^LWResultEphemerisCallback)(NSString * _Nullable object, NSError * _Nullable error);
+
+
+/**
+ 文件上传协商结果回调
+ @param object              协商结果
+ @param error               错误信息，为nil时表示成功
+ */
+typedef void(^LWResultUploadNegotCallback)(LWUploadNegotModel * _Nullable object, NSError * _Nullable error);
+
+
+#endif /* LWCallback_h */
---
+++ SDK/LinWearKit.framework/Headers/LWHeader.h
@@ -0,0 +1,47 @@
+//
+//  LWHeader.h
+//  LinWearKit
+//
+//  Created by LINWEAR on 2025-12-16.
+//
+
+#ifndef LWHeader_h
+#define LWHeader_h
+
+/**
+ 框架名称：LinWearKit.xcframework
+ 框架功能：智能设备的iOS框架，负责与智能设备通信等功能的封装。
+                             
+ GitHub @link https://github.com/wsr1949/LinWearKit.git
+ 
+ 版本记录：
+ 
+ project    2025-12-27  Version:1.0.1   Build:2025122701
+            1.新增请求离线语音授权码 参@link requestOfflineVoiceAuthCodeWithMac:withLang:withCallback:
+ 
+ project    2025-12-20  Version:1.0.0   Build:2025122001
+            1.首版
+ */
+
+#import <CoreBluetooth/CoreBluetooth.h>
+#import <LinWearKit/LWMacro.h>
+
+#import <LinWearKit/LWPeripheralModel.h>
+#import <LinWearKit/LWDeviceConfigModel.h>
+#import <LinWearKit/LWAipetStatusModel.h>
+#import <LinWearKit/LWAipetActionModel.h>
+#import <LinWearKit/LWAipetEvolvedModel.h>
+#import <LinWearKit/LWPoiCheckInModel.h>
+#import <LinWearKit/LWAipetRewardModel.h>
+#import <LinWearKit/LWTodayWeatherModel.h>
+#import <LinWearKit/LWHourWeatherModel.h>
+#import <LinWearKit/LWAnimationModel.h>
+#import <LinWearKit/LWHolidayAnimationModel.h>
+#import <LinWearKit/LWUploadFileModel.h>
+#import <LinWearKit/LWUploadFileNegotModel.h>
+#import <LinWearKit/LWUploadNegotModel.h>
+
+#import <LinWearKit/LWDelegate.h>
+#import <LinWearKit/LWCallback.h>
+
+#endif /* LWHeader_h */
